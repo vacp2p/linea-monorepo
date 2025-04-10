f4b10c6e4a6c29b2e81fc069b438b47134a70315
Andrea Franz <andrea@gravityblast.com>
2025-04-10 12:07:22 +0200

    feat(yield): import Blast's yield manager

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -48,6 +48,28 @@ const config: HardhatUserConfig = {
           evmVersion: "prague",
         },
       },
+      {
+        version: "0.8.28",
+        settings: {
+          viaIR: useViaIR,
+          optimizer: {
+            enabled: true,
+            runs: 10_000,
+          },
+          evmVersion: "cancun",
+        },
+      },
+      {
+        version: "0.8.15",
+        settings: {
+          viaIR: useViaIR,
+          optimizer: {
+            enabled: true,
+            runs: 10_000,
+          },
+          evmVersion: "london",
+        },
+      },
     ],
     overrides: overrides,
   },
