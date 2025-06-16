f6ebdcde96027a6acdd6a900c4966869513d61cc
Andrea Franz <andrea@gravityblast.com>
2025-06-16 14:56:32 +0200

    chore: add L1/L2 ETHBridge interfaces and emit events when addresses are updated

---
+++ contracts/src/yield/bridge/interfaces/IL1ETHBridge.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.26;
+
+interface IL1ETHBridge {
+  /**
+   * @notice Emitted when the message service address is set.
+   * @param newMessageService The indexed new message service address.
+   * @param oldMessageService The indexed old message service address.
+   * @param setBy The indexed address setting the new message service address.
+   */
+  event MessageServiceUpdated(
+    address indexed newMessageService,
+    address indexed oldMessageService,
+    address indexed setBy
+  );
+
+  /**
+   * @notice Emitted when the yield manager address is set.
+   * @param newYieldManager The indexed new yield manager address.
+   * @param oldYieldManager The indexed old yield manager address.
+   * @param setBy The indexed address setting the new yield manager address.
+   */
+  event YieldManagerUpdated(
+    address indexed newYieldManager,
+    address indexed oldYieldManager,
+    address indexed setBy
+  );
+
+  error L1ETHBridge__ZeroValueNotAllowed();
+  error L1ETHBridge__ZeroAddressNotAllowed();
+  error L1ETHBridge__YieldManagerDepositFailed();
+
+  function setRemoteSender(address _remoteSender) external;
+
+  function setMessageService(address _messageService) external;
+
+  function setYieldManager(address _yieldManager) external;
+
+  function bridgeETH(address _to, bytes memory _calldata) external payable;
+}
