c0fbac57e5eb7ae21b5e053294b1c847a88b0537
Andrea Franz <andrea@gravityblast.com>
2025-06-26 15:42:56 +0200

    chore: removed unused IRollup interface

--- contracts/src/bridging/eth/interfaces/IRollup.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.30;
-
-interface IRollup {
-  function sendMessage(address _to, uint256 _fee, bytes calldata _calldata) external payable;
-}
