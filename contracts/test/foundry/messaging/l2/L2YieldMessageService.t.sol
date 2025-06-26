c54f856960af8c33107b887f032912e5a8e7ca8b
Andrea Franz <andrea@gravityblast.com>
2025-06-26 10:26:57 +0200

    chore: move L2YieldMessageService under src/messaging/l2

--- contracts/test/foundry/messaging/l2/L2YieldMessageService.t.sol
+++ contracts/test/foundry/messaging/l2/L2YieldMessageService.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.30;
 
 import { Test } from "forge-std/Test.sol";
 import { L2MessageService } from "../../../../src/messaging/l2/L2MessageService.sol";
-import { L2YieldMessageService } from "../../../../src/yield/bridge/L2YieldMessageService.sol";
+import { L2YieldMessageService } from "../../../../src/messaging/l2/L2YieldMessageService.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import { IPermissionsManager } from "../../../../src/security/access/interfaces/IPermissionsManager.sol";
 import { IPauseManager } from "../../../../src/security/pausing/interfaces/IPauseManager.sol";
